bfb693c4e0ffef49df3af4b5b2fd75ec77544a1f
Credit <credit.qe@gmail.com>
2025-07-29 01:30:28 +0900

    feat:create my portfolio

---
+++ components/sub/project-card.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
+type ProjectCardProps = {
+  src: string;
+  title: string;
+  description: string;
+  link: string;
+};
+
+export const ProjectCard = ({
+  src,
+  title,
+  link,
+}: ProjectCardProps) => {
+  return (
+    <Link
+      href={link}
+      target="_blank"
+      rel="noreferrer noopener"
+      className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]"
+    >
+      <Image
+        src={src}
+        alt={title}
+        width={1000}
+        height={1000}
+        className="w-full object-contain"
+      />
+
+      <div className="relative p-4">
+        <h3 className="text-2xl font-semibold text-white">{title}</h3>
+        {/* <p className="mt-2 text-gray-300">{description}</p> */}
+      </div>
+    </Link>
+  );
+};
---
+++ constants/index.ts
@@ -0,0 +1,344 @@
+import { FaYoutube, FaFacebook } from "react-icons/fa";
+import {
+  RxDiscordLogo,
+  RxGithubLogo,
+  RxInstagramLogo,
+  RxTwitterLogo,
+  RxLinkedinLogo,
+} from "react-icons/rx";
+
+export const SKILL_DATA = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    image: "redux.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React Query",
+    image: "reactquery.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Framer Motion",
+    image: "framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Stripe",
+    image: "stripe.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+] as const;
+
+// export const SOCIALS = [
+//   {
+//     name: "Instagram",
+//     icon: RxInstagramLogo,
+//     link: "https://instagram.com",
+//   },
+//   {
+//     name: "Facebook",
+//     icon: FaFacebook,
+//     link: "https://facebook.com",
+//   },
+//   {
+//     name: "Twitter",
+//     icon: RxTwitterLogo,
+//     link: "https://twitter.com",
+//   },
+// ] as const;
+
+export const FRONTEND_SKILL = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Material UI",
+    image: "mui.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    image: "redux.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React Query",
+    image: "reactquery.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const BACKEND_SKILL = [
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Express.js",
+    image: "express.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+  {
+    skill_name: "Firebase",
+    image: "firebase.png",
+    width: 55,
+    height: 55,
+  },
+  {
+    skill_name: "PostgreSQL",
+    image: "postgresql.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "MySQL",
+    image: "mysql.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Prisma",
+    image: "prisma.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Graphql",
+    image: "graphql.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const FULLSTACK_SKILL = [
+  {
+    skill_name: "React Native",
+    image: "reactnative.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Tauri",
+    image: "tauri.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Docker",
+    image: "docker.png",
+    width: 70,
+    height: 70,
+  },
+
+  {
+    skill_name: "Figma",
+    image: "figma.png",
+    width: 50,
+    height: 50,
+  },
+] as const;
+
+export const OTHER_SKILL = [
+  {
+    skill_name: "Go",
+    image: "go.png",
+    width: 60,
+    height: 60,
+  },
+] as const;
+
+export const PROJECTS = [
+  {
+    title: "カリキュラムコース|PLATCLOVER",
+    // description:
+    //   '日本語学校で実施しているカリキュラムをご用意！カリキュラムは4か国語対応！',
+    image: "/projects/1.png",
+    link: "https://www.plat-clover.com/",
+  },
+  {
+    title: "中古トラックの丸山自動車",
+    // description:
+    //   'Step into the extraordinary world of my professional journey through the "Interactive Cards Portfolio" - an innovative and visually captivating platform that redefines the traditional portfolio experience. Ditching the conventional static layout, this portfolio leverages interactive cards to showcase my skills, projects, and personality in an engaging and dynamic manner.',
+    image: "/projects/2.png",
+    link: "https://truckichi-soka.co.jp/wp/",
+  },
+  {
+    title: "SALES CROSS",
+    // description:
+    //   'Embark on an interstellar journey with my "Space Themed Website", a mesmerizing space-themed website that invites you to explore the cosmic wonders beyond our world. Immerse yourself in an awe-inspiring digital experience that blends cutting-edge design with the mysteries of the universe.',
+    image: "/projects/3.png",
+    link: "https://sales-cross.com/",
+  },
+    {
+    title: "Elite Rentals | Discover Your Perfect Ride",
+    // description:
+    //   'Embark on an interstellar journey with my "Space Themed Website", a mesmerizing space-themed website that invites you to explore the cosmic wonders beyond our world. Immerse yourself in an awe-inspiring digital experience that blends cutting-edge design with the mysteries of the universe.',
+    image: "/projects/4.png",
+    link: "https://elite-rentals.vercel.app/",
+  },
+      {
+    title: "BikeKart",
+    // description:
+    //   'BikeKart is just another bike store web template with mulitple sections.',
+    image: "/projects/5.png",
+    link: "https://itsvijaysingh.github.io/BikeKart/",
+  },
+] as const;
+
+export const FOOTER_DATA = [
+  {
+    title: "Community",
+    data: [
+      {
+        name: "GitHub",
+        icon: RxGithubLogo,
+        link: "https://github.com/biztex",
+      },
+      {
+        name: "Discord",
+        icon: RxDiscordLogo,
+        link: "https://discord.gg/MBafQTFY",
+      },
+    ],
+  },
+
+  {
+    title: "About",
+    data: [
+      {
+        name: "Contact Me",
+        icon: null,
+        link: "credit.qe@gmail.com",
+      }
+    ],
+  },
+] as const;
+
+export const NAV_LINKS = [
+  {
+    title: "About me",
+    link: "#about-me",
+  },
+  {
+    title: "Skills",
+    link: "#skills",
+  },
+  {
+    title: "Projects",
+    link: "#projects",
+  },
+] as const;
+
+export const LINKS = {
+  sourceCode: "https://github.com/biztex",
+};
