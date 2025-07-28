2cefd07287da79a861fe3aa28b090dcaaba54903
Credit <credit.qe@gmail.com>
2025-07-29 02:43:57 +0900

    updated: navbar.js is updated

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-import { LINKS, NAV_LINKS, SOCIALS } from "@/constants";
+import { LINKS, NAV_LINKS} from "@/constants";
 
 export const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
